ceb5670d063c33bca1b0bc82e2dbe6bff6dcc94a
Tanya Bihag <tbihag@gmail.com>
2019-08-22 18:16:31 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,13 +8,6 @@ sentence2 = "Hi again, just making sure it's reversed!"
   reverse_array = new_arr.join(" ")
    return reverse_array.reverse
   reverse_array.collect do |sentence|
-    
-end
-
-
-
-
-  it 'uses collect' do
-    expect_any_instance_of(Array).to receive(:collect).and_call_original
-    reverse_each_word("Verifying that collect is being called.")
+    return "Verifying that collect is being called."
   end
+end
