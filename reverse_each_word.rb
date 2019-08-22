3eee34e21939f9e8cf45dd68909ef71342e4c009
Tanya Bihag <tbihag@gmail.com>
2019-08-22 18:41:39 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,11 +2,7 @@ sentence1 = "Hello there, and how are you?"
 sentence2 = "Hi again, just making sure it's reversed!"
 
 def reverse_each_word(sentence1)
-  split_string = sentence1.split().each do |sentence|
-    sentence.reverse!
-  end
-  split_string.join(" ")
-  split_string = sentence1.collect().each do |sentence|
+  split_string = sentence1.split().collect do |sentence|
     sentence.reverse!
   end
   split_string.join(" ")
