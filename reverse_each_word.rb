0eb07c21f9cb5e9dce5590305f35c7342c1649d1
Tanya Bihag <tbihag@gmail.com>
2019-08-22 18:39:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,7 +8,6 @@ def reverse_each_word(sentence1)
   split_string.join(" ")
 end
 
-reverse_each_word("Verifying that collect is being called.")
 
 def reverse_each_word(sentence1)
   split_string = sentence1.collect().each do |sentence|
