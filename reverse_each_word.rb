d288377da33ac1d4c8158ec5846c4f88bd7bad39
Tanya Bihag <tbihag@gmail.com>
2019-08-22 18:41:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,11 +6,6 @@ def reverse_each_word(sentence1)
     sentence.reverse!
   end
   split_string.join(" ")
-  
-end
-
-
-def reverse_each_word(sentence1)
   split_string = sentence1.collect().each do |sentence|
     sentence.reverse!
   end
