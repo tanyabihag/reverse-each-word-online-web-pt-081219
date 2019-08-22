01bff2e48d69c4bf5edbd4d352fe81423ca31e38
Tanya Bihag <tbihag@gmail.com>
2019-08-22 18:09:30 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,5 +8,6 @@ sentence2 = "Hi again, just making sure it's reversed!"
   reverse_array = new_arr.join(" ")
    return reverse_array.reverse
   new_arr.collect do |sentence1|
+    return reverse_array
   end
 end
